c3ade5c4b26abc78b2630a40335e981ca381f000
Gereltuya <gereltuya.altangerel@gmail.com>
2023-02-23 20:55:28 +0100

    env, echo test

--- env/minishell_test.c
+++ env/minishell_test.c
@@ -2,17 +2,20 @@
 
 void    ft_env(char **envp)
 {
-    int i;
-
-    i = 0;
-    
     while (*envp)
     {
-
-
+        printf("%s\n", *envp);
+        *envp++;
     }
 }
 
+void    ft_echo(char *line)
+{
+    if (strlen(line) == 4)
+        printf("\n");
+    else
+        printf("im working on echo!\n");
+}
 
 int main    (int argc, char **argv, char **envp)
 {
@@ -30,6 +33,11 @@ int main    (int argc, char **argv, char **envp)
     {
         if (!strcmp(line, "env"))
             ft_env(envp);
+        else if (!strncmp(line, "echo", 4))
+        {
+            ft_echo(line);
+            // printf("%s\n", line);
+        }
         else
             printf("im not ready for this O.o\n");
         line = readline("test: ");
